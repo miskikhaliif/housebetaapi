44985e8a792cbb32195851c5b138d92421f04e47
misk khaliif <miskabdiraxman383@gmail.com>
2023-07-27 02:53:51 -0700

    connection error wala xaliye

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
+require("dotenv").config()
 const dbconnect = async () =>{
     try {
-        mongoose.connect(process.env.DB_CONNECT)
-        console.log("is connected")
+     return await   mongoose.connect(process.env.DB_CONNECT)
+        // console.log("is connected")
     } catch (error) {
         console.log("not connected")
     }
